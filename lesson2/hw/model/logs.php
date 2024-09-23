72a80eec9c62817003e17200e843eb1a76812b2e
Mikle Seregin <mikhail-seregin@yandex.ru>
2024-09-23 10:24:44 +0300

    lesson 2 end

--- lesson2/hw/model/logs.php
+++ lesson2/hw/model/logs.php
@@ -74,6 +74,7 @@ function checkFileName(string $name): bool
     return !!preg_match('/^\d{4}\-\d{2}\-\d{2}\.txt$/', $name);
 }
 
+
 /**
  * Checks if the given file name is a valid log file in the 'logs' directory.
  *

7fe0e70416c838c13a112d532c8c1bc69fa2ef73
Mikle Seregin <mikhail-seregin@yandex.ru>
2024-10-04 17:20:27 +0300

    lesson 3 start

--- lesson1/3-get.php
+++ lesson1/3-get.php
@@ -6,3 +6,13 @@
 
 	$id = (int)($_GET['id'] ?? '');
 	echo $id;
+    ?>
+
+<script>
+     fetch('/php/ajax.php',{
+            method: 'POST',
+            body: new FormData(this.form)
+        }).then(response => response.json()).then(function (data){
+            console.log(data);
+        }).catch(function(err) {console.log('Fetch Error :-S', err);});
+</script>

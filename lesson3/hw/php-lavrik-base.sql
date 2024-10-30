3ff67469c7fb436a9c399545cbd567e3bae6bd3c
Mikle Seregin <mikhail-seregin@yandex.ru>
2024-10-30 15:02:14 +0300

    lesson 3 hw update

--- lesson3/hw/php-lavrik-base.sql
+++ lesson3/hw/php-lavrik-base.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Окт 28 2024 г., 13:54
+-- Время создания: Окт 30 2024 г., 14:07
 -- Версия сервера: 10.8.4-MariaDB
--- Версия PHP: 8.1.9
+-- Версия PHP: 7.4.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -28,19 +28,19 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `articles` (
-  `article_id` int(11) UNSIGNED NOT NULL,
+  `id_article` int(11) UNSIGNED NOT NULL,
   `title` varchar(256) NOT NULL,
   `content` text NOT NULL,
   `dt` timestamp NOT NULL DEFAULT current_timestamp(),
-  `user_id` int(11) UNSIGNED NOT NULL,
-  `category_id` int(11) UNSIGNED NOT NULL
+  `id_user` int(11) UNSIGNED NOT NULL,
+  `id_category` int(11) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Дамп данных таблицы `articles`
 --
 
-INSERT INTO `articles` (`article_id`, `title`, `content`, `dt`, `user_id`, `category_id`) VALUES
+INSERT INTO `articles` (`id_article`, `title`, `content`, `dt`, `id_user`, `id_category`) VALUES
 (1, 'Первая статья', 'Voluptate excepteur elit labore irure commodo voluptate quis fugiat culpa cupidatat. Et id aute laboris veniam aute irure est reprehenderit commodo cupidatat fugiat ullamco. Excepteur voluptate occaecat tempor ea. Magna ex id dolor amet velit. Magna anim ipsum ipsum exercitation amet quis id enim dolor.\r\nAliquip incididunt duis labore tempor deserunt culpa qui ut officia qui enim eu Lorem. Qui anim exercitation tempor labore incididunt ullamco commodo ad duis nisi nisi. Occaecat ut culpa velit consequat. Sit aliquip aute non excepteur tempor laboris pariatur.', '2024-10-28 10:50:59', 1, 4),
 (2, 'Вторая статья модератора', 'Voluptate excepteur elit labore irure commodo voluptate quis fugiat culpa cupidatat. Et id aute laboris veniam aute irure est reprehenderit commodo cupidatat fugiat ullamco. Excepteur voluptate occaecat tempor ea. Magna ex id dolor amet velit. Magna anim ipsum ipsum exercitation amet quis id enim dolor.\r\nAliquip incididunt duis labore tempor deserunt culpa qui ut officia qui enim eu Lorem. Qui anim exercitation tempor labore incididunt ullamco commodo ad duis nisi nisi. Occaecat ut culpa velit consequat. Sit aliquip aute non excepteur tempor laboris pariatur.', '2024-10-28 10:50:59', 2, 1),
 (3, 'Третья статья модератора', 'Voluptate excepteur elit labore irure commodo voluptate quis fugiat culpa cupidatat. Et id aute laboris veniam aute irure est reprehenderit commodo cupidatat fugiat ullamco. Excepteur voluptate occaecat tempor ea. Magna ex id dolor amet velit. Magna anim ipsum ipsum exercitation amet quis id enim dolor.\r\nAliquip incididunt duis labore tempor deserunt culpa qui ut officia qui enim eu Lorem. Qui anim exercitation tempor labore incididunt ullamco commodo ad duis nisi nisi. Occaecat ut culpa velit consequat. Sit aliquip aute non excepteur tempor laboris pariatur.', '2024-10-28 10:51:38', 2, 2),
@@ -53,15 +53,15 @@ INSERT INTO `articles` (`article_id`, `title`, `content`, `dt`, `user_id`, `cate
 --
 
 CREATE TABLE `article_tags` (
-  `article_id` int(11) UNSIGNED NOT NULL,
-  `tag_id` int(11) UNSIGNED NOT NULL
+  `id_article` int(11) UNSIGNED NOT NULL,
+  `id_tag` int(11) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Дамп данных таблицы `article_tags`
 --
 
-INSERT INTO `article_tags` (`article_id`, `tag_id`) VALUES
+INSERT INTO `article_tags` (`id_article`, `id_tag`) VALUES
 (1, 1),
 (1, 2),
 (2, 1),
@@ -74,19 +74,20 @@ INSERT INTO `article_tags` (`article_id`, `tag_id`) VALUES
 --
 
 CREATE TABLE `categories` (
-  `category_id` int(11) UNSIGNED NOT NULL,
-  `name` varchar(256) NOT NULL
+  `id_category` int(11) UNSIGNED NOT NULL,
+  `title` varchar(256) NOT NULL,
+  `url` varchar(256) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Дамп данных таблицы `categories`
 --
 
-INSERT INTO `categories` (`category_id`, `name`) VALUES
-(1, 'Спорт'),
-(2, 'Образование'),
-(3, 'Здоровье'),
-(4, 'Новости');
+INSERT INTO `categories` (`id_category`, `title`, `url`) VALUES
+(1, 'Спорт', ''),
+(2, 'Образование', ''),
+(3, 'Здоровье', ''),
+(4, 'Новости', '');
 
 -- --------------------------------------------------------
 
@@ -95,19 +96,20 @@ INSERT INTO `categories` (`category_id`, `name`) VALUES
 --
 
 CREATE TABLE `tags` (
-  `tag_id` int(11) UNSIGNED NOT NULL,
-  `name` varchar(256) NOT NULL
+  `id_tag` int(11) UNSIGNED NOT NULL,
+  `title` varchar(256) NOT NULL,
+  `url` varchar(256) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Дамп данных таблицы `tags`
 --
 
-INSERT INTO `tags` (`tag_id`, `name`) VALUES
-(1, 'Травмы'),
-(2, 'Победы'),
-(3, 'Праздник'),
-(4, 'Победа');
+INSERT INTO `tags` (`id_tag`, `title`, `url`) VALUES
+(1, 'Травмы', ''),
+(2, 'Победы', ''),
+(3, 'Праздник', ''),
+(4, 'Победа', '');
 
 -- --------------------------------------------------------
 
@@ -116,7 +118,7 @@ INSERT INTO `tags` (`tag_id`, `name`) VALUES
 --
 
 CREATE TABLE `users` (
-  `user_id` int(11) UNSIGNED NOT NULL,
+  `id_user` int(11) UNSIGNED NOT NULL,
   `name` varchar(256) NOT NULL,
   `password` varchar(256) NOT NULL,
   `dt` timestamp NOT NULL DEFAULT current_timestamp()
@@ -126,7 +128,7 @@ CREATE TABLE `users` (
 -- Дамп данных таблицы `users`
 --
 
-INSERT INTO `users` (`user_id`, `name`, `password`, `dt`) VALUES
+INSERT INTO `users` (`id_user`, `name`, `password`, `dt`) VALUES
 (1, 'Админ', '123456', '2024-10-28 10:47:42'),
 (2, 'Модератор', '654321', '2024-10-28 10:47:42');
 
@@ -138,34 +140,34 @@ INSERT INTO `users` (`user_id`, `name`, `password`, `dt`) VALUES
 -- Индексы таблицы `articles`
 --
 ALTER TABLE `articles`
-  ADD PRIMARY KEY (`article_id`),
-  ADD KEY `user_id` (`user_id`),
-  ADD KEY `category_id` (`category_id`);
+  ADD PRIMARY KEY (`id_article`),
+  ADD KEY `user_id` (`id_user`),
+  ADD KEY `category_id` (`id_category`);
 
 --
 -- Индексы таблицы `article_tags`
 --
 ALTER TABLE `article_tags`
-  ADD PRIMARY KEY (`article_id`,`tag_id`),
-  ADD KEY `tag_id` (`tag_id`);
+  ADD PRIMARY KEY (`id_article`,`id_tag`),
+  ADD KEY `tag_id` (`id_tag`);
 
 --
 -- Индексы таблицы `categories`
 --
 ALTER TABLE `categories`
-  ADD PRIMARY KEY (`category_id`);
+  ADD PRIMARY KEY (`id_category`);
 
 --
 -- Индексы таблицы `tags`
 --
 ALTER TABLE `tags`
-  ADD PRIMARY KEY (`tag_id`);
+  ADD PRIMARY KEY (`id_tag`);
 
 --
 -- Индексы таблицы `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`user_id`);
+  ADD PRIMARY KEY (`id_user`);
 
 --
 -- AUTO_INCREMENT для сохранённых таблиц
@@ -175,25 +177,25 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT для таблицы `articles`
 --
 ALTER TABLE `articles`
-  MODIFY `article_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_article` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT для таблицы `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `category_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_category` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT для таблицы `tags`
 --
 ALTER TABLE `tags`
-  MODIFY `tag_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_tag` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT для таблицы `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_user` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Ограничения внешнего ключа сохраненных таблиц
@@ -203,15 +205,15 @@ ALTER TABLE `users`
 -- Ограничения внешнего ключа таблицы `articles`
 --
 ALTER TABLE `articles`
-  ADD CONSTRAINT `articles_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`),
-  ADD CONSTRAINT `articles_ibfk_2` FOREIGN KEY (`category_id`) REFERENCES `categories` (`category_id`);
+  ADD CONSTRAINT `articles_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`),
+  ADD CONSTRAINT `articles_ibfk_2` FOREIGN KEY (`id_category`) REFERENCES `categories` (`id_category`);
 
 --
 -- Ограничения внешнего ключа таблицы `article_tags`
 --
 ALTER TABLE `article_tags`
-  ADD CONSTRAINT `article_tags_ibfk_1` FOREIGN KEY (`article_id`) REFERENCES `articles` (`article_id`),
-  ADD CONSTRAINT `article_tags_ibfk_2` FOREIGN KEY (`tag_id`) REFERENCES `tags` (`tag_id`);
+  ADD CONSTRAINT `article_tags_ibfk_1` FOREIGN KEY (`id_article`) REFERENCES `articles` (`id_article`),
+  ADD CONSTRAINT `article_tags_ibfk_2` FOREIGN KEY (`id_tag`) REFERENCES `tags` (`id_tag`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
